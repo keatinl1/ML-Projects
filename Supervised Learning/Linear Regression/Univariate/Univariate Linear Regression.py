4ab82a7137dbda7874121b80bc0d4792240619c6
Luke <keatinl1@tcd.ie>
2021-08-19 14:58:24 +0100

    changed dataset, last one was too small

--- Supervised Learning/Linear Regression/Univariate/Univariate Linear Regression.py
+++ Supervised Learning/Linear Regression/Univariate/Univariate Linear Regression.py
@@ -2,7 +2,6 @@
 #
 # Done using PyTorch
 
-
 import torch 
 import torch.nn as nn
 import pandas as pd
@@ -11,21 +10,23 @@ import matplotlib.pyplot as plt
 
 
 # 1 - Import Data
-data = pd.read_csv("heightsandweights.csv")
-# print(data) # uncomment to see data
+data = pd.read_csv("dataset.csv")
+print(data) # uncomment to see data
 
-X_numpy, y_numpy = data.Height, data.Weight
+X_numpy, y_numpy = data.x, data.y
 X_numpy, y_numpy = np.array(X_numpy), np.array(y_numpy)
 
 plt.plot(X_numpy, y_numpy, 'ro')
-#plt.show() # uncomment to see the plot before line fitting
+plt.show() # uncomment to see the plot before line fitting
 
 # 2 - Make tensors from np.arrays
 X = torch.from_numpy(X_numpy.astype(np.float32))
 y = torch.from_numpy(y_numpy.astype(np.float32))
 X = X.view(X.shape[0], 1)
+y = y.view(y.shape[0], 1)
 
 n_samples, n_features = X.shape
+
 
 # 3 - Model
 input_size = n_features
@@ -35,13 +36,13 @@ model = nn.Linear(input_size, output_size)
 
 
 # 4 - Loss and optimiser
-learning_rate = 0.005
+learning_rate = 0.01
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate)
 
 
 # 5 - Training Loop
-num_epochs = 500
+num_epochs = 100
 for epoch in range(num_epochs):
     # forward pass and loss
     y_predicted = model(X)
@@ -58,12 +59,12 @@ for epoch in range(num_epochs):
     if (epoch+1) % 10 == 0:
         print(f'epoch: {epoch+1}, loss = {loss.item():.2f}')
 
-    
+
 
 # 6 - Plot
 
 predicted = model(X).detach()
 plt.plot(X_numpy, predicted, 'b')
-plt.show()
+#plt.show()
 
 
