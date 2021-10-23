ba374ca757f117a5ae5c258484036b923bd8f114
Luke <keatinl1@tcd.ie>
2021-10-23 21:21:17 +0100

    improving the wine

--- Supervised Learning/Multivariate Linear Regression/Multivariate Linear Regression.py
+++ Supervised Learning/Multivariate Linear Regression/Multivariate Linear Regression.py
@@ -7,48 +7,48 @@
 # Dataset available at: 
 # https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/
 
-
-# 0 - Setup & data import
+# 0 - Imports
 
 import pandas as pd
-pd.options.mode.chained_assignment = None
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.datasets import make_blobs
 
-# contains dictionaries that let me change from month & day to number:
-# from helper import monthToNum, dayToNum 
+data = pd.read_csv("winequality-red.txt", sep=";")
+#print(data.head()) 
 
-data = pd.read_csv("wine-quality.txt", sep=";")
-print(data.head()) 
+# 1 - Split data
 
 X = data.drop(['quality'], axis='columns')
 y = data.quality
 
-# 1 - Data Pre- processing
-
-
-
-
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) 
 
 # 2 - Model
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) 
 
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-
-# 3 - Predictions & accuracy
-
-y_pred = model.predict(X_test)
+# 3 - Accuracy
 
 score = model.score(X_test, y_test) # score makes predictions with X_test and compares it to y_test
 formatted_score = "{:.2f}".format(score)
 print('Model score is:' , formatted_score)
 
-error = mean_squared_error(y_test, y_pred)
-formatted_error = "{:.2f}".format(error)
-print('The model mean squared error is:', formatted_error)
+# 4 - Ask user for imput
+
+'''
+user_wine=[]  
+print("Please enter the 11 feature values")
+for i in range(11):  
+    Xnew.append(float(input())) 
+Xnew = np.array([user_wine])
+print('User generated wine:\n', user_wine)
+'''
+
+new_wine, _ = make_blobs(n_samples=1, centers=2, n_features=11, random_state=1) 
+#print('Randomly generated wine:\n', new_wine)
+
+y_pred = model.predict(new_wine)
+print("Predicted wine quality: ", y_pred)
